d8c7acd21915c9cbe225f3308bf0b3e0d83f7da3
littlelittleJessica <446067382@qq.com>
2021-11-19 19:38:28 +0800

    学术交流——留学申请 页面及交互测试完成

---
+++ src/views/admin/academic/academic_study_abroad.vue
@@ -0,0 +1,301 @@
+<template>
+  <h3>学术交流资源更新——留学申请</h3>
+  <p class="title">留学申请资源目录</p>
+
+  <el-button class="fresh"
+             @click="ListShowSlide"
+  >刷新
+  </el-button>
+
+  <el-button class="add"
+             @click="add"
+  >新增
+  </el-button>
+  <!--新增时的弹出框表单-->
+  <el-dialog v-model="addFormVisible" title="新增留学申请内容">
+    <el-form v-model="uploadFile">
+
+      <el-form-item label="文件" prop="image">
+        <big-file ref="uploadComp"
+                  v-model:image="uploadFile.image"
+                  v-bind:category="'00000404'"
+                  v-bind:file-type='["jpg", "jpeg", "png","mp4"]'/>
+      </el-form-item>
+
+      <el-form-item label="文案" prop="text">
+        <el-input autocomplete="off"
+                  :rows="4"
+                  type="textarea"
+                  style="margin-top: 10px"
+                  v-model="uploadFile.text"></el-input>
+      </el-form-item>
+
+    </el-form>
+    <template #footer>
+            <span class="dialog-footer">
+              <el-button @click="addFormVisible = false">取消</el-button>
+              <el-button type="primary" @click="saveFile">保存</el-button>
+            </span>
+    </template>
+  </el-dialog>
+
+  <!--显示所有图文的表格-->
+  <el-table :data="tableData" stripe border>
+    <el-table-column label="id" prop="id" width="90px" align="center"/>
+    <el-table-column label="图片" width="120px" align="center" prop="image">
+      <template v-slot="scope">
+        <img :src="scope.row.image" width="100" height="70" align="center"/>
+      </template>
+    </el-table-column>
+    <el-table-column label="文案" prop="text" width="400px" align="center"/>
+    <el-table-column label="操作" prop="operation" align="center">
+      <template v-slot="scope">
+        <el-button size="mini" class="normal-button" @click="edit(scope.row)">编辑</el-button>
+        <el-button size="mini" class="normal-button" @click="jumpToDetail(scope.row)">详情</el-button>
+
+        <el-popconfirm
+              confirm-button-text="确定"
+              cancel-button-text="取消"
+              title="确认删除该条图文？"
+              @confirm="deleteFile(scope.row)"
+        >
+          <template #reference>
+            <el-button
+                  size="mini"
+                  type="danger"
+            >删除
+            </el-button>
+          </template>
+        </el-popconfirm>
+      </template>
+    </el-table-column>
+  </el-table>
+
+  <!--编辑时弹出的模态框-->
+  <el-dialog v-model="editFormVisible" title="编辑首页介绍部分轮播图文">
+
+    <el-form :data="formData">
+      <el-form-item label="id" prop="id" style="margin-top: 10px; vertical-align: middle">
+        <el-input autocomplete="off"
+                  :rows="1"
+                  type="text"
+                  disabled
+                  v-model="formData.id"
+        ></el-input>
+      </el-form-item>
+
+      <el-form-item label="文件" prop="image">
+        <big-file ref="uploadComp"
+                  v-model:image="formData.image"
+                  v-bind:category="'00000404'"
+                  v-bind:file-type='["jpg", "jpeg", "png"]'/>
+      </el-form-item>
+      <el-form-item label="文案" prop="text" style="margin-top: 10px; vertical-align: middle">
+        <el-input autocomplete="off"
+                  :rows="1"
+                  type="text"
+                  v-model="formData.text"
+        ></el-input>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+            <span class="dialog-footer">
+              <el-button @click="editFormVisible = false">取消</el-button>
+              <el-button type="primary" @click="saveEdit">保存</el-button>
+            </span>
+    </template>
+  </el-dialog>
+
+</template>
+
+<script lang="ts">
+  import {reactive, defineComponent, onMounted, ref} from 'vue';
+  import axios from "axios";
+  import {ElMessage} from 'element-plus';
+  import BigFile from '@/components/bigFile.vue';
+  import router from '@/router/index';
+
+  declare let SessionStorage: any;
+
+  export default defineComponent({
+    name: "academic_study_abroad",
+    components: {
+      BigFile
+    },
+
+
+    setup: function () {
+      const showData = ref();//定义展示的数据
+      showData.value = [];
+
+      const tableData = ref();//定义表格数据
+      tableData.value = [];
+
+      const addFormVisible = ref(false);
+      const editFormVisible = ref(false);
+
+      // 显示全部轮播图文
+      const ListAllSlide = () => {
+        axios.get("http://127.0.0.1:9000/business/admin/academic-study-abroad/list").then((response) => {
+          const data = response.data;
+          //读取数据
+          if (data.success) {
+            tableData.value = data.content
+          } else {
+            ElMessage.error(data.message);
+          }
+        });
+      };
+
+
+      //新增轮播图文，打开模态框表单
+      const add = () => {
+        addFormVisible.value = true;
+      }
+      const uploadFile = reactive({
+        image: '',
+        text: '',
+        category: '00000404',
+      });
+      //保存新增
+      const saveFile = () => {
+        console.log(uploadFile);
+        axios.post('http://127.0.0.1:9000/business/admin/academic-study-abroad/save', {
+          image: uploadFile.image,
+          text: uploadFile.text,
+          category: uploadFile.category
+        }).then((response) => {
+          const data = response.data;
+          if (data.success) {
+            ElMessage.success("新增成功！")
+            uploadFile.image = '';
+            uploadFile.category = '';
+            uploadFile.text = '';
+            clearImage();
+            addFormVisible.value = false;
+            ListAllSlide();
+          } else {
+            ElMessage.error(data.message);
+          }
+        });
+      };
+      //保存后清除上传图片框中的图片
+      const uploadComp = ref();
+      const clearImage = () => {
+        uploadComp.value.clearFiles();
+      }
+
+
+      const formData = reactive({
+        id: '',
+        image: '',
+        text: ''
+      });//定义表单数据
+      //编辑轮播图文，打开表单，表单赋值
+      const edit = (row: any) => {
+        formData.id = row.id;
+        formData.image = row.image;
+        formData.text = row.text;
+        editFormVisible.value = true;
+      };
+      //保存编辑
+      const saveEdit = () => {
+        axios.post('http://127.0.0.1:9000/business/admin/academic-study-abroad/save', {
+          id: formData.id,
+          image: formData.image,
+          text: formData.text
+        }).then((response) => {
+          const data = response.data;
+          if (data.success) {
+            ElMessage.success("更新轮播图文成功！")
+            formData.image = '';
+            formData.text = '';
+            clearImage();
+            editFormVisible.value = false;
+            ListAllSlide();
+          } else {
+            ElMessage.error(data.message);
+          }
+        });
+      }
+
+      //删除轮播图文
+      const deleteFile = (row: any) => {
+        axios.get('http://127.0.0.1:9000/business/admin/academic-study-abroad/delete/' + row.id).then((response) => {
+          const data = response.data;
+          if (data.success) {
+            ElMessage.success("删除成功！")
+            ListAllSlide();
+          } else {
+            ElMessage.error("删除失败！")
+          }
+        })
+      }
+
+
+      //跳转到活动详情
+      const jumpToDetail = (row: any) => {
+        SessionStorage.set("slideId", row.id);
+        router.push("/detail/study-abroad/detail");
+        console.log("跳转详情，id：" + row.id);
+      }
+
+      onMounted(() => {
+        ListAllSlide();
+      });
+
+      return {
+        showData,
+        tableData,
+        ListAllSlide,
+        add,
+        addFormVisible,
+        editFormVisible,
+        edit,
+        deleteFile,
+        saveFile,
+        uploadFile,
+        formData,
+        saveEdit,
+        uploadComp,
+        jumpToDetail
+      }
+    }
+
+  });
+</script>
+
+<style scoped>
+  h3 {
+    font-family: Tahoma;
+  }
+
+  .title {
+    font-family: Tahoma;
+    font-weight: bold;
+    font-size: 14px;
+    color: black;
+    text-shadow: 3px 3px 3px #bdc2ff;
+  }
+
+  .add {
+    color: #868EFF;
+    border-color: #868EFF;
+    margin-left: 30px;
+    margin-bottom: 10px;
+  }
+
+  .fresh {
+    color: #868EFF;
+    border-color: #868EFF;
+    margin-left: 80%;
+    margin-bottom: 10px;
+  }
+
+  .normal-button {
+    background-color: rgba(212, 212, 255, 0.4);
+    border-color: #868EFF;
+    color: #757dff;
+  }
+
+</style>
---
+++ src/views/admin/academic/study_abroad_detail.vue
@@ -0,0 +1,263 @@
+<template>
+  <p class="academic-study-abroad-title" style="margin-bottom: 20px">留学申请文案编辑</p>
+
+  <div id="academic-study-abroad-allSlides" class="demo-collapse">
+    <el-collapse accordion>
+      <el-collapse-item>
+        <template #title>
+          <span class="academic-study-abroad-title">显示全部图片视频文件</span>
+        </template>
+
+        <div>
+          <el-button class="add"
+                     @click="addFile"
+          >
+            新增文件
+          </el-button>
+        </div>
+        <!--新增时的弹出框表单-->
+        <el-dialog v-model="addFormVisible" title="新增详情内容文件">
+          <el-form v-model="uploadFile">
+
+            <el-form-item label="文件" prop="image">
+              <big-file ref="uploadComp"
+                        v-model:image="uploadFile.image"
+                        v-bind:category="'00000404'"
+                        v-bind:file-type='["jpg", "jpeg", "png","mp4"]'/>
+            </el-form-item>
+
+          </el-form>
+          <template #footer>
+            <span class="dialog-footer">
+              <el-button @click="addFormVisible = false">取消</el-button>
+              <el-button type="primary" @click="saveFile">保存</el-button>
+            </span>
+          </template>
+        </el-dialog>
+
+
+        <!--显示所有图文的表格-->
+        <el-table :data="tableData" stripe border id="table">
+          <el-table-column label="id" prop="id" width="90px" align="center"/>
+          <el-table-column label="url" prop="url" width="700px" align="center"/>
+          <el-table-column label="图片/视频" prop="image" width="120px" align="center" >
+            <template v-slot="scope">
+              <img :src="scope.row.url" width="100" height="70" align="center"/>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" prop="operation" align="center">
+            <template v-slot="scope">
+              <el-popconfirm
+                    confirm-button-text="确定"
+                    cancel-button-text="取消"
+                    title="确认删除该图片？"
+                    @confirm="deleteFile(scope.row)"
+              >
+                <template #reference>
+                  <el-button
+                        size="mini"
+                        type="danger"
+                  >删除
+                  </el-button>
+                </template>
+              </el-popconfirm>
+            </template>
+          </el-table-column>
+        </el-table>
+
+
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+
+
+  <!--富文本框-->
+  <div>
+    <div id="content"></div>
+    <el-button class="normal-button" style="margin-top: 10px" @click="saveContent">保存</el-button>
+    <el-button type="danger" style="margin-top: 10px">取消</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+  import {reactive, defineComponent, onMounted, ref} from 'vue';
+  import E from 'wangeditor';
+  import axios from "axios";
+  import {ElMessage} from 'element-plus';
+  import bigFile from "@/components/bigFile.vue";
+  import {Tool} from "@/util/tool";
+  import router from '@/router/index';
+
+  declare let SessionStorage: any;
+
+  export default defineComponent({
+    name: "forum_detail",
+    components: {
+      bigFile
+    },
+
+    setup() {
+
+      const slide_id = ref();
+
+      const addFormVisible = ref(false);
+      //新增轮播图文，打开模态框表单
+      const addFile = () => {
+        addFormVisible.value = true;
+      }
+      const uploadFile = reactive({
+        slideId: '',
+        image: '',
+      });
+      //保存新增文件
+      const saveFile = () => {
+        axios.post('http://127.0.0.1:9000/business/admin/academic-study-abroad-file/save', {
+          slideId: slide_id.value,
+          url: uploadFile.image,
+        }).then((response) => {
+          const data = response.data;
+          if (data.success) {
+            ElMessage.success("新增文件成功！")
+            uploadFile.image = '';
+            clearImage();
+            ListAllFile(slide_id.value);
+            addFormVisible.value = false;
+          } else {
+            ElMessage.error(data.message);
+          }
+        });
+      };
+      //保存后清除上传图片框中的图片
+      const uploadComp = ref();
+      const clearImage = () => {
+        uploadComp.value.clearFiles();
+      }
+
+      const tableData = ref();//定义表格数据
+      tableData.value = [];
+
+      // 显示全部文件
+      const ListAllFile = (slideId: any) => {
+        axios.get("http://127.0.0.1:9000/business/admin/academic-study-abroad-file/list/" + slideId).then((response) => {
+          const data = response.data;
+          //读取数据
+          if (data.success) {
+            for (let i = 0; i < data.content.length; i++) {
+              let fileName = data.content[i]["url"];
+              let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, fileName.length).toLowerCase();
+              if (suffix === "mp4") {
+                data.content[i]["url"] = '<iframe src="' + data.content[i]["url"] + '"></iframe>';
+              }
+            }
+            tableData.value = data.content
+          } else {
+            ElMessage.error(data.message);
+          }
+        });
+      };
+
+      //删除文件
+      const deleteFile = (row: any) => {
+        axios.get('http://127.0.0.1:9000/business/admin/academic-study-abroad-file/delete/' + row.id).then((response) => {
+          const data = response.data;
+          if (data.success) {
+            ElMessage.success("删除成功！")
+            ListAllFile(slide_id.value);
+          } else {
+            ElMessage.error("删除失败！")
+          }
+        })
+      }
+
+
+      //富文本框赋值
+      const editor = new E('#content');
+      editor.config.zIndex = 0; //修改覆盖层级
+      const doc = ref();
+      doc.value = {};
+
+      //查询文章内容
+      const showContent = (slideId: any) => {
+        axios.get('http://127.0.0.1:9000/business/admin/academic-study-abroad-content/show/' + slideId).then((response) => {
+          const data = response.data;
+          if (data.success) {
+            editor.txt.html(data.content);
+          }
+        })
+      }
+
+      //保存文章内容
+      const saveContent = () => {
+        doc.value.content = editor.txt.html();
+        axios.post('http://127.0.0.1:9000/business/admin/academic-study-abroad-content/save', {
+          id: slide_id.value,
+          content: doc.value.content
+        }).then((response) => {
+          const data = response.data;
+          if (data.success) {
+            ElMessage.success("保存文章内容成功！")
+          } else {
+            ElMessage.error(data.message);
+          }
+        });
+      }
+
+
+      onMounted(() => {
+        let slideId = ref();
+        slideId.value = SessionStorage.get("slideId") || {};
+        if (Tool.isEmpty(slideId.value)) {
+          router.push("/home/academic/study-abroad")
+        }
+        slide_id.value = slideId.value;
+        ListAllFile(slideId.value);
+        showContent(slideId.value)
+        editor.create();
+      });
+
+      return {
+        addFile,
+        saveFile,
+        uploadFile,
+        uploadComp,
+        addFormVisible,
+        tableData,
+        ListAllFile,
+        deleteFile,
+        saveContent
+      }
+    }
+  });
+</script>
+
+<style scoped>
+  .academic-study-abroad-title {
+    font-family: Tahoma;
+    font-weight: bold;
+    font-size: 14px;
+    color: black;
+    text-shadow: 3px 3px 3px #bdc2ff;
+  }
+
+  .add {
+    color: #868EFF;
+    border-color: #868EFF;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+
+  #table {
+    margin-bottom: 20px
+  }
+
+  #academic-study-abroad-allSlides {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+</style>
+
+<style>
+  .w-e-text-container {
+    height: 700px !important;
+  }
+</style>
